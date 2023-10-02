aa15d8c7a92fb6ae562b6dca3bd78e815f078811
mohdakhlad <127624324+mohdakhlad@users.noreply.github.com>
2023-10-02 11:49:33 +0530

    first commit

---
+++ src/main/java/com/sailotech/mavenproject/App.java
@@ -0,0 +1,13 @@
+package com.sailotech.mavenproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
